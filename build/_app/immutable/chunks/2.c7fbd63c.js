1f44898b76f7deefad3e59dcf22db437c56595ae
spencerm <spnzrr@gmail.com>
2023-04-07 23:13:33 -0400

    add build

---
+++ build/_app/immutable/chunks/2.c7fbd63c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.2c8f90a5.js";export{t as component};
--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
   kit: {
     adapter: adapter(),
     paths: {
-      base: dev ? '' : '/spencerm/svelte-test',
+      base: dev ? '' : '/spencerm/spencerm.github.io',
     }
   }
 };
